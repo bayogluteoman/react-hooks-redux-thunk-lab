603daf053ed87ab1ed100ea5844b94bda8a7873d
bayogluteoman <bayogluteoman@gmail.com>
2022-03-03 03:29:33 +0300

    make an async web request with use redux-thunk middleware

--- src/index.js
+++ src/index.js
@@ -2,5 +2,16 @@ import React from "react";
 import ReactDOM from "react-dom";
 import App from "./App";
 import "bootstrap/dist/css/bootstrap.min.css";
+import { Provider } from "react-redux";
+import { createStore, applyMiddleware } from "redux";
+import catsReducer from "./features/cats/catsSlice";
+import thunk from "redux-thunk";
+import { composeWithDevTools } from "redux-devtools-extension";
 
-ReactDOM.render(<App />, document.getElementById("root"));
+const store = createStore(catsReducer, composeWithDevTools(applyMiddleware(thunk)))
+
+ReactDOM.render(
+    <Provider store={store}>
+        <App />
+    </Provider>,
+    document.getElementById("root"));
